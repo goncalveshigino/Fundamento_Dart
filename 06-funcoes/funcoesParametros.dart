e2ca78b8f035da290af84b51fb6abf74ec0277f1
goncalveshigino <goncalveshigino20@gmail.com>
2022-07-24 13:06:57 +0100

    chore funcoes arroe e com parametros

--- 06-funcoes/funcoesParametros.dart
+++ 06-funcoes/funcoesParametros.dart
@@ -1,4 +1,3 @@
-
 ///
 ///Funcoes com parametros posicionados devem obeder a ordem declarada
 ///Funcoes com parametros nomeados deve ser declarado
@@ -18,11 +17,25 @@ funcaoParamentros() {
   print('\n06.3.2) Funcoes  com parametros nomeados e default\n');
 
   exibirDados2(String nome, {int peso = 87, double? altura}) {
-    print('Nome: $nome, Peso: $peso, Altura: ${ altura ?? 'Não informada!'}');
+    print('Nome: $nome, Peso: $peso, Altura: ${altura ?? 'Não informada!'}');
   }
-  
-   exibirDados1('Katia');
-  exibirDados2('Katia',  peso: 87);
+
+  exibirDados1('Katia');
+  exibirDados2('Katia', peso: 87);
+
+  print('\n06.3.3) Funcoes como parametros para outras funcoes\n');
+
+  void falar() {
+    print('Essa é uma fução passada como parametro nomeado');
+  }
+
+  void saudacao(String nome, {required Function funcaoFalar}) {
+    print('Olá eu sou: $nome!');
+    funcaoFalar();
+  }
+
+  saudacao('Higino Katiavala', funcaoFalar: falar);
+  saudacao('Higino Katiavala', funcaoFalar: () => print('Essa é uma fução anonima'));
 }
 
 void main() {
